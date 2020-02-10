a06fb881e8a5e5bb9e677732872d221ef74a6329
pstar98 <saber100.saber100@gmail.com>
2020-02-10 18:39:51 +0330

    LoginController

--- src/main/java/ir/hasan/bimarestammospring/controoller/PersonController.java
+++ src/main/java/ir/hasan/bimarestammospring/controoller/PersonController.java
@@ -3,12 +3,13 @@ package ir.hasan.bimarestammospring.controoller;
 import ir.hasan.bimarestammospring.model.PersonEntity;
 import ir.hasan.bimarestammospring.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/people")
@@ -17,26 +18,27 @@ public class PersonController {
     @Autowired
     private PersonRepository personRepository;
 
-    @GetMapping
-    public List<PersonEntity> getAllPeople(@RequestParam(required = false) String firstName) {
-        if (firstName != null ) {
-            return personRepository.findByFirstName(firstName);
+    @GetMapping("/p1")
+    public Object getAllPeople(@RequestParam(required = false) String id) {
+        if (id != null ) {
+
+            return personRepository.findById(id);
         } else {
             List<PersonEntity> people = new ArrayList<>();
             personRepository.findAll().forEach(people::add);
             return people;
         }
     }
-
-    @GetMapping("/{id}")
-    public PersonEntity getPerson(@PathVariable String id) {
-        Optional<PersonEntity> personOptional = personRepository.findById(id);
-        if (personOptional.isPresent()) {
-            return personOptional.get();
-        } else {
-            throw new RuntimeException("Person not found");
-        }
-    }
+  
+//    @GetMapping("/{id}")
+//    public PersonEntity getPerson(@PathVariable String id) {
+//        Optional<PersonEntity> personOptional = personRepository.findById(id);
+//        if (personOptional.isPresent()) {
+//            return personOptional.get();
+//        } else {
+//            throw new RuntimeException("Person not found");
+//        }
+//    }
 
     @PostMapping
     public PersonEntity savePersonEntity(@RequestBody PersonEntity personEntity) {
--- src/main/java/ir/hasan/bimarestammospring/repository/PersonRepository.java
+++ src/main/java/ir/hasan/bimarestammospring/repository/PersonRepository.java
@@ -3,10 +3,10 @@ package ir.hasan.bimarestammospring.repository;
 import ir.hasan.bimarestammospring.model.PersonEntity;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface PersonRepository extends CrudRepository<PersonEntity, String> {
 
 
-    List<PersonEntity> findByFirstName(String firstName);
+    Optional<PersonEntity> findById(String id);
 }
